30c61fee164014a0810ebba4b0171a7d27e40a15
nit-ka <103489048+nit-ka@users.noreply.github.com>
2022-05-05 10:22:18 +0200

    Add files via upload

---
+++ day8_caesar_cipher.py
@@ -0,0 +1,44 @@
+logo = """           
+ ,adPPYba, ,adPPYYba,  ,adPPYba, ,adPPYba, ,adPPYYba, 8b,dPPYba,  
+a8"     "" ""     `Y8 a8P_____88 I8[    "" ""     `Y8 88P'   "Y8  
+8b         ,adPPPPP88 8PP"""""""  `"Y8ba,  ,adPPPPP88 88          
+"8a,   ,aa 88,    ,88 "8b,   ,aa aa    ]8I 88,    ,88 88          
+ `"Ybbd8"' `"8bbdP"Y8  `"Ybbd8"' `"YbbdP"' `"8bbdP"Y8 88   
+            88             88                                 
+           ""             88                                 
+                          88                                 
+ ,adPPYba, 88 8b,dPPYba,  88,dPPYba,   ,adPPYba, 8b,dPPYba,  
+a8"     "" 88 88P'    "8a 88P'    "8a a8P_____88 88P'   "Y8  
+8b         88 88       d8 88       88 8PP""""""" 88          
+"8a,   ,aa 88 88b,   ,a8" 88       88 "8b,   ,aa 88          
+ `"Ybbd8"' 88 88`YbbdP"'  88       88  `"Ybbd8"' 88          
+              88                                             
+              88           
+"""
+alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+
+caesar_on = True
+
+def caesar(user_text, shift_amount, direction_choice,):
+    end_text = ""
+    if direction_choice == "decode":
+      shift_amount *= -1
+    for char in user_text:
+      if char in alphabet:
+        new_position = abs(alphabet.index(char) + shift_amount) % 26
+        new_letter = alphabet[new_position]
+        end_text += new_letter
+      else:
+        end_text += char
+    print(f"The {direction_choice}d text is {end_text}")
+
+print(logo)
+while caesar_on:
+  direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n")
+  text = input("Type your message:\n").lower()
+  shift = int(input("Type the shift number:\n"))
+  caesar(text, shift, direction)
+  continue_decision = input("Type 'yes' if you want to go again, type 'no' to quit.\n")
+  if continue_decision == "no":
+    caesar_on = False
+    print("Goodbye")
