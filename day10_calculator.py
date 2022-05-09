07ff6ddcf2cdf92c57131fec494aa5ed9edfb9f3
nit-ka <103489048+nit-ka@users.noreply.github.com>
2022-05-09 09:03:08 +0200

    Add files via upload

---
+++ day10_calculator.py
@@ -0,0 +1,57 @@
+#Calculator
+logo = """
+ _____________________
+|  _________________  |
+| | Pythonista   0. | |  .----------------.  .----------------.  .----------------.  .----------------. 
+| |_________________| | | .--------------. || .--------------. || .--------------. || .--------------. |
+|  ___ ___ ___   ___  | | |     ______   | || |      __      | || |   _____      | || |     ______   | |
+| | 7 | 8 | 9 | | + | | | |   .' ___  |  | || |     /  \     | || |  |_   _|     | || |   .' ___  |  | |
+| |___|___|___| |___| | | |  / .'   \_|  | || |    / /\ \    | || |    | |       | || |  / .'   \_|  | |
+| | 4 | 5 | 6 | | - | | | |  | |         | || |   / ____ \   | || |    | |   _   | || |  | |         | |
+| |___|___|___| |___| | | |  \ `.___.'\  | || | _/ /    \ \_ | || |   _| |__/ |  | || |  \ `.___.'\  | |
+| | 1 | 2 | 3 | | x | | | |   `._____.'  | || ||____|  |____|| || |  |________|  | || |   `._____.'  | |
+| |___|___|___| |___| | | |              | || |              | || |              | || |              | |
+| | . | 0 | = | | / | | | '--------------' || '--------------' || '--------------' || '--------------' |
+| |___|___|___| |___| |  '----------------'  '----------------'  '----------------'  '----------------' 
+|_____________________|
+"""
+
+def add(n1, n2):
+  return n1 + n2
+
+def subtract(n1, n2):
+  return n1 - n2
+
+def multiply(n1, n2):
+  return n1 * n2
+
+def divide(n1, n2):
+  return n1 / n2
+
+math_operations = {
+  "+": add,
+  "-": subtract,
+  "*": multiply,
+  "/": divide,  
+}
+
+def calculator():
+  print(logo)
+  num1 = float(input("What's the first number?: "))
+  for symbol in math_operations:
+    print(symbol)
+  should_continue = True
+  while should_continue:
+    operation_symbol = input("Pick an operation: ")
+    num2 = float(input("What's the next number?: "))
+    function = math_operations[operation_symbol]
+    result = function(num1, num2)
+    print(f"{num1} {operation_symbol} {num2} = {result}")
+    decision_continue = input(f"Type 'y' to continue calculating with {result}, or type 'n' to start a new calculation.: ")
+    if decision_continue == 'y':
+      num1 = result
+    else:
+      should_continue = False
+      calculator()
+
+calculator()
